4fd6fd96a7176bf748a1353526f66ea5791efaa0
Vidhi Patel <vidhibpatel94@gmail.com>
2021-09-25 13:02:45 -0400

    Designed step 1 screen.

---
+++ app/src/main/java/com/example/ecogeekfestapp/Step1Activity.java
@@ -0,0 +1,14 @@
+package com.example.ecogeekfestapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Step1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
